018051d83d57e2179e1fddfb39909539aa6f3505
Ahmad Siddiq <ahmadsiddiqpali@mail.com>
2020-04-09 17:10:11 +0700

    cart screen

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -17,16 +17,25 @@ const Home = () => {
         />
             <div className="header">
                 <img src={require('../asset/img/menu.png')} alt="" className="imgMenu" />
-                <h1 className="titleList">List Item</h1>
-                <input type="text" className={transInput ? 'inputSearch active' : 'inputSearch'} />
+                <h1 className="titleList title">List Items</h1>
+                <input type="text" placeholder="search" className={transInput ? 'inputSearch active' : 'inputSearch'} />
                 <img className="imgSearch" onClick={()=> setTrans(transInput ? false : true)} src={require('../asset/img/search.png')} alt=""/>
                 <div className="cartHeader">
-                    <h1>Cart</h1>
+                    <h1 className="title">Cart</h1>
+                    <div className="elipse">10</div>
                 </div>
             </div>
-            <div className="sideBar" style={{height: height}}></div>
+            <div className="sideBar" style={{height: height}}>
+                <img className="iconBar" src={require('../asset/img/spoon.png')} alt=""/>
+                <img className="iconBar" src={require('../asset/img/catalog.png')} alt=""/>
+                <img className="iconBar" src={require('../asset/img/add.png')} alt=""/>
+            </div>
             
-            <div className="cartBar"style={{height: height}}></div>
+            <div className="cartBar" style={{height: height}}>
+                <img className="iconEmpty" src={require('../asset/img/food-and-restaurant.png')} alt=""/>
+                <h3>Your cart is empty</h3>
+                <p className="textCart">Please add some items from the menu</p>
+            </div>
             <div className="content">
                 <img src={require('../asset/img/wiener.png')} alt=""/>
             </div>
